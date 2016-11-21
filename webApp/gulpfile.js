ec79846eca4173b1f0f20b1660379b80f805563d
Tobias Steidle <tobias.steidle@softwaredev.de>
2016-11-21 16:07:16 +0100

    add gulp as task runner + add jasmine as js testframework

---
+++ webApp/gulpfile.js
@@ -0,0 +1,18 @@
+const gulp = require('gulp');
+const jasmine = require('gulp-jasmine');
+const jasmineBrowser = require('gulp-jasmine-browser');
+const watch = require('gulp-watch');
+
+gulp.task('default', function() {
+    gulp.src('src/test/javascript/**/*.js')
+        .pipe(jasmine())
+});
+
+gulp.task('jasmine', function() {
+    var filesForTest = ['src/test/javascript/**/*.js'];
+    var filesToWatch = ['src/main/resources/**/*.js', 'src/test/javascript/**/*.js'];
+    return gulp.src(filesForTest)
+        .pipe(watch(filesToWatch))
+        .pipe(jasmineBrowser.specRunner())
+        .pipe(jasmineBrowser.server({port: 8888}));
+});
